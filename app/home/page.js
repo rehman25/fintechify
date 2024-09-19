b0341d73011298236c0c74b1b8af121e484bd367
Hamza8559 <programmingsavedata@gmail.com>
2024-09-18 17:29:27 -0700

    hamza

--- app/home/page.js
+++ app/home/page.js
@@ -1,10 +1,12 @@
 import Footer from "../components/headerAndFooter/Footer";
 import Header from "../components/headerAndFooter/Header";
+import InquirySection from "../components/Inquire/Inquire";
 
 export default function page() {
   return (
    <>
    <Header />
+   <InquirySection />
    <Footer />
    </>
   );
