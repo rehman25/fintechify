950bba98825246eda1c52c78ae975c492a4ef1bf
Hamza8559 <programmingsavedata@gmail.com>
2024-09-27 09:22:41 -0700

    hamza

--- app/contact-us/page.js
+++ app/contact-us/page.js
@@ -5,6 +5,8 @@ import Footer from '../components/Footer/Footer'
 import Banner from '../components/Banner/Banner'
 import styles from '../components/assets/css/contact.module.css'
 import { MdOutlineAddIcCall } from "react-icons/md";
+import ContactImg from '../components/assets/images/contact-img.jpg'
+import Image from 'next/image'
 
 
 function page() {
@@ -45,6 +47,44 @@ function page() {
                                     <span>Email</span>
                                     <a href="">talktous@fintechify.ae</a>
                                 </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className={styles.contactFormSection}>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-6">
+                            <div className={styles.contactFormImg}>
+                                <Image src={ContactImg}/>
+                            </div>
+                        </div>
+                        <div className="col-lg-6">
+                            <div className={styles.contactFormBox}>
+                                <h5>Get In Touch</h5>
+                                <p>Your email address will not be published. Required fields are marked *</p>
+                                <form>
+                                    <div class="form-group">
+                                        <label>Name *</label>
+                                        <input type="text" class="form-control" required placeholder="Enter Your Name" />
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Email Address *</label>
+                                        <input type="email" class="form-control" required placeholder="Your Email" />
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Subject *</label>
+                                        <input type="text" class="form-control" required placeholder="Message Subject" />
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Comments / Questions *</label>
+                                        <textarea class="form-control" rows="3"></textarea>
+                                    </div>
+                                    <div class="form-group">
+                                       <button>Post Message</button>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
