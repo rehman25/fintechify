ea5e54a5c7c0e13fe3583ee9b7a7ffabff7cc1eb
Hamza8559 <programmingsavedata@gmail.com>
2024-10-11 16:57:25 -0700

    hamza

--- app/components/bannerHome/banner.jsx
+++ app/components/bannerHome/banner.jsx
@@ -80,6 +80,63 @@ const Banner = () => {
             </div>
           </div>
         )}
+
+        {isTwo && (
+          <div className={`row ${style.slider1}`}>
+            <div className={style.rowContent}>
+              <div className={`col-lg-6 col-md-6 m-0 p-0 ${style.colleft2}`}>
+                <h1 className={style.BannerText} data-aos="fade-up" data-aos-duration="1000">
+                Empowering Financial Futures
+                </h1>
+                <div className={style.picture}>
+                  <div className={style.people} data-aos="fade-up" data-aos-duration="1500">
+                    <Image src={head1} alt="Head 1" className={style.head1} />
+                    <Image src={head2} alt="Head 2" className={style.head2} />
+                    <Image src={head3} alt="Head 3" className={style.head3} />
+                    <span>
+                      <h4>29.5M</h4>
+                      <h6>Most Active User</h6>
+                    </span>
+                  </div>
+                  <div className={style.star}>
+                    <Image src={head4} alt="Head 4" className={style.head4} />
+                    <span>
+                      <h6>2024 The World</h6>
+                      <h6>Best Digital Bank</h6>
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className={`col-lg-4 col-md-4 m-0 p-0 ${style.colright2}`}>
+                <Image src={Phone} alt="Phone" className={style.phoner} />
+                <p data-aos="fade-up" data-aos-duration="1500">
+                Welcome to Fintechify, your gateway to cutting-edge financial solutions in the heart of the UAE. We revolutionize the way you bank, offering a seamless blend of technology and financial solutions to meet the unique needs of our customers.
+                </p>
+                <button data-aos="fade-up" data-aos-duration="1500">
+                  Learn More
+                  <span>
+                    <MdArrowOutward />
+                  </span>
+                </button>
+              </div>
+            </div>
+
+            <div className={`row m-0 ${style.btmRow}`}>
+              <div className={`${style.card1}`}></div>
+              <div className={`${style.card2}`}></div>
+              <div className={`${style.card3}`}></div>
+              <div className={`${style.card4}`}></div>
+            </div>
+
+            <Image src={Phone1} alt="Phone1" className={style.phoner1} />
+
+            <div className={`row m-0 ${style.btmRow2}`}>
+              <div className={`${style.card5}`}></div>
+              <div className={`${style.card6}`}></div>
+              <div className={`${style.card7}`}></div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
